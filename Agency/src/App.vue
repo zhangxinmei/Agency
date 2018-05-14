4a129dbe6dc8eae6df0c2bf9651f80f9524648dd
zhangxinmei <2536075245@qq.com>
2018-05-15 00:32:54 +0800

    add files

---
+++ Agency/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="agency">
+      <div class="agency-hd">
+        <h1 class="hd-title">供销商合同设定</h1>
+        <retrieval @alertTip="showAlert"></retrieval>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import retrieval from "./components/Retrieval/Retrieval";
+export default {
+  name: "App",
+  components: {
+    retrieval
+  },
+  methods: {
+    showAlert() {
+      alert(32323);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "./style/common/reset.less";
+.agency-hd {
+  padding: 20px;
+  .hd-title {
+    font-size: 20px;
+    font-weight: 400;
+  }
+}
+</style>
+
---
+++ Agency/src/components/Retrieval/Retrieval.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="retrieval">
+    <el-row type="flex" class="row-bg" justify="space-between">
+      <el-col :span="12">
+        <div class="grid-content retrieval-ft">
+          公司名称：<el-input v-model="cName" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content retrieval-rt">
+          是否拜访：<el-select v-model="visit" placeholder="All">
+            <el-option
+              v-for="item in visitOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visitOptions:[{
+        value:'0',
+        label:'是',
+      },{
+        value:'1',
+        label:'否',
+      },{
+        value:'2',
+        label:'All',
+      }],
+      cName: "",
+      visit:'',
+    };
+  },
+  methods: {
+    alertTip() {
+      this.$emit("alertTip");
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.retrieval {
+  padding: 10px 0;
+  max-width: 1000px;
+  margin: 0 auto;
+  background-color: #eee;
+}
+.el-input {
+  position: relative;
+  font-size: 14px;
+  display: inline-block;
+  width: 180px;
+}
+</style>
