016d919d9aad27d4e7993b4b7f0ff1fe81cfdca7
zhangxinmei <2536075245@qq.com>
2018-05-16 00:24:25 +0800

    add

--- Agency/src/App.vue
+++ Agency/src/App.vue
@@ -2,10 +2,21 @@
   <div id="app">
     <div class="agency">
       <div class="agency-hd">
-        <h1 class="hd-title">供销商合同设定</h1>
+        <h1 class="hd-title">标题</h1>
         <retrieval @alertTip="showAlert"></retrieval>
       </div>
-      
+      <div class="list">
+        <ul>
+          <li v-for="(item,index) in listOption2" :key="index" :id="item.id" @click="selectItem(item)">{{item.text}}</li>
+        </ul>
+      </div>
+      <el-button type="primary" @click="btnClick">主要按钮</el-button>
+      <div class="detail">
+        <div>
+          <span>{{itemOptions.id}}</span>
+          <span>{{itemOptions.text}}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,9 +28,45 @@ export default {
   components: {
     retrieval
   },
+  data() {
+    return {
+      listOption1:[{
+        id:1,
+        text:'苹果'
+      },{
+        id:2,
+        text:'香蕉'
+      },],
+      listOption2:[],
+      itemOptions:{},
+    }
+  },
+  watch:{
+   
+    btnwatchClick() {
+      this.showList()
+    }
+   
+  },
+  created() {
+    this.showList()
+  },
   methods: {
     showAlert() {
       alert(32323);
+    },
+    showList() {
+      this.listOption2=this.listOption1
+     
+    },
+    btnClick() {
+      this.listOption1.push({id:3,text:'橘子'})
+      console.log('listOption1',this.listOption1)
+      console.log('listOption2',this.listOption2)
+    },
+    selectItem(item) {
+      this.itemOptions=item
+      console.log(555,this.itemOptions)
     }
   }
 };
